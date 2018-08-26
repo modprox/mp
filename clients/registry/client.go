e68f42560adc28fe24c3e276072e6dc602b35b98
Seth Hoenig <hoenig@indeed.com>
2018-08-26 14:30:03 -0500

    enable the registry client to work using default http protocol

--- clients/registry/client.go
+++ clients/registry/client.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/modprox/libmodprox/loggy"
+
 	"github.com/pkg/errors"
 	"github.com/shoenig/toolkit"
 
@@ -27,46 +29,68 @@ type Options struct {
 	Timeout   time.Duration
 }
 
-func NewClient(options Options) Client {
-	return &client{
-		options: options,
-	}
-}
-
 type client struct {
 	options    Options
 	httpClient *http.Client
+	log        loggy.Logger
+}
+
+func NewClient(options Options) Client {
+	return &client{
+		options: options,
+		httpClient: &http.Client{
+			Timeout: options.Timeout,
+		},
+		log: loggy.New("registry-client"),
+	}
 }
 
 func (c *client) ModInfos() ([]repository.ModInfo, error) {
 	path := "/v1/registry/sources/list"
 	modInfos := make([]repository.ModInfo, 0, 100)
-	err := c.get(path, modInfos)
+	err := c.get(path, &modInfos)
 	return modInfos, err
 }
 
 func (c *client) get(path string, i interface{}) error {
 	for _, addr := range c.options.Addresses {
 		if err := c.getSingle(path, addr, i); err != nil {
+			c.log.Warnf("GET request failed for %s: %v", addr, err)
+			continue // keep trying with the next address
+		} else {
+			// the request was a success, can stop trying now
 			return nil
 		}
 	}
 	return errors.Errorf("failed to GET from any registry: %v", c.options.Addresses)
 }
 
-func (c *client) getSingle(path string, addr netutil.Service, i interface{}) error {
-	url := fmt.Sprintf("%s:%d/%s", addr.Address, addr.Port, strings.TrimPrefix(path, "/"))
+// maybe set this in configuration somewhere
+func tweak(addr string) string {
+	if !strings.HasPrefix(addr, "http") {
+		return "http://" + addr
+	}
+	return addr
+}
 
+func (c *client) getSingle(path string, addr netutil.Service, i interface{}) error {
+	url := fmt.Sprintf("%s:%d/%s", tweak(addr.Address), addr.Port, strings.TrimPrefix(path, "/"))
+
+	c.log.Tracef("GET single for url %q", url)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
+		c.log.Errorf("GET single create request had error: %v", err)
 		return err
 	}
 
 	response, err := c.httpClient.Do(request)
 	if err != nil {
+		c.log.Errorf("GET single request had error: %v", err)
 		return err
 	}
 	defer toolkit.Drain(response.Body)
+
+	c.log.Tracef("GET single response code: %d", response.StatusCode)
 
 	if response.StatusCode >= 400 {
 		bs, err := ioutil.ReadAll(response.Body)
