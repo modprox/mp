5ab6013c715cccde5f179b104bf4955f156a73bf
Seth Hoenig <hoenig@indeed.com>
2018-08-28 23:29:07 -0500

    provide basic string method for modinfo struct

--- repository/modinfo.go
+++ repository/modinfo.go
@@ -1,6 +1,12 @@
 package repository
 
+import "fmt"
+
 type ModInfo struct {
 	Source  string `json:"source"`
 	Version string `json:"version"`
+}
+
+func (mi ModInfo) String() string {
+	return fmt.Sprintf("(%s @ %s)", mi.Source, mi.Version)
 }
