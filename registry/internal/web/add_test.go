bd17b57910fe809b01941cb1c86732fb618d496c
Seth Hoenig <hoenig@indeed.com>
2018-09-11 14:50:21 -0500

    issue #38 : parse module lines that contain a @ in them

--- registry/internal/web/add_test.go
+++ registry/internal/web/add_test.go
@@ -9,10 +9,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func compareErr(t *testing.T, expErr, gotErr error) {
+	if expErr == nil {
+		require.Nil(t, gotErr)
+	} else {
+		require.NotNil(t, gotErr)
+		require.Equal(t, expErr.Error(), gotErr.Error())
+	}
+}
+
 func Test_parseLine(t *testing.T) {
 	try := func(input string, exp Parsed) {
 		parsed := parseLine(input)
-		require.Equal(t, exp, parsed)
+		require.Equal(t, exp.Text, parsed.Text)
+		require.Equal(t, exp.Module, parsed.Module)
+		compareErr(t, exp.Err, parsed.Err)
 	}
 
 	try( // malformed
@@ -20,7 +31,7 @@ func Test_parseLine(t *testing.T) {
 		Parsed{
 			Text:   "github.com/foo/bar",
 			Module: coordinates.Module{},
-			Err:    errors.New("malformed module line"),
+			Err:    errors.New(`malformed module line: "github.com/foo/bar"`),
 		},
 	)
 
@@ -43,6 +54,18 @@ func Test_parseLine(t *testing.T) {
 			Module: coordinates.Module{
 				Source:  "github.com/foo/bar",
 				Version: "v0.0.0-20180111040409-fbec762f837d",
+			},
+			Err: nil,
+		},
+	)
+
+	try( // with @ notation
+		"github.com/kr/pty@v1.1.1",
+		Parsed{
+			Text: "github.com/kr/pty@v1.1.1",
+			Module: coordinates.Module{
+				Source:  "github.com/kr/pty",
+				Version: "v1.1.1",
 			},
 			Err: nil,
 		},
