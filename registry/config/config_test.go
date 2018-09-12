87920359a5f2cefa4bdd924df30715c574cb5a62
Seth Hoenig <hoenig@indeed.com>
2018-09-11 19:31:44 -0500

    issue #48 : fix CSRF configuration tests

--- registry/config/config_test.go
+++ registry/config/config_test.go
@@ -1,12 +1,41 @@
 package config
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
 
-func TestConfiguration_MustCSRFAuthKey(t *testing.T) {
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Configuration_CSRF_nokey(t *testing.T) {
 	var c Configuration
-	assert.Panics(t, func() { c.MustCSRFAuthKey() }, "should panic with invalid csrf_auth_key")
-	c.CSRFAuthKey = "this is a 32 byte long string ok"
-	assert.Len(t, c.CSRFAuthKey, 32)
-	assert.NotPanics(t, func() { c.MustCSRFAuthKey() }, "should not panic with valid csrf_auth_key")
+	_, _, err := c.CSRF.Key()
+	require.Error(t, err)
+}
+
+func Test_Configuration_CSRF_badkey(t *testing.T) {
+	var c Configuration
+	c.CSRF.AuthenticationKey = "foobar"
+	_, _, err := c.CSRF.Key()
+	require.Error(t, err)
+}
+
+func Test_Configuration_CSRF_devmode(t *testing.T) {
+	key := "12345678901234567890123456789012"
+	var c Configuration
+	c.CSRF.DevelopmentMode = true
+	c.CSRF.AuthenticationKey = key
+	bs, devMode, err := c.CSRF.Key()
+	require.NoError(t, err)
+	require.True(t, devMode)
+	require.Equal(t, []byte(key), bs)
+}
+
+func Test_Configuration_CSRF(t *testing.T) {
+	key := "12345678901234567890123456789012"
+	var c Configuration
+	c.CSRF.AuthenticationKey = key
+	bs, devMode, err := c.CSRF.Key()
+	require.NoError(t, err)
+	require.False(t, devMode)
+	require.Equal(t, []byte(key), bs)
 }
