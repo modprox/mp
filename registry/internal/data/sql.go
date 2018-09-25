dd93a97c0c382501104436c688b84badd4f33309
Seth Hoenig <hoenig@indeed.com>
2018-09-25 00:17:50 -0500

    issue #81 : fix postgresql statements that got broken

--- registry/internal/data/sql.go
+++ registry/internal/data/sql.go
@@ -13,8 +13,8 @@ const (
 	selectModuleIDScanSQL
 	selectModulesByIDsSQL
 	selectSourcesScanSQL
-	insertStartupConfigSQL
 	insertHeartbeatSQL
+	insertStartupConfigSQL
 	selectStartupConfigsSQL
 	selectHeartbeatsSQL
 )
@@ -22,15 +22,15 @@ const (
 type statements map[int]*sql.Stmt
 
 func load(kind string, db *sql.DB) (statements, error) {
-	loaded := make(statements, len(mySQLText))
+	loaded := make(statements, len(mySQLTexts))
 
-	stmtText := mySQLText
+	stmtTexts := mySQLTexts
 	if kind == "postgres" {
-		stmtText = postgreSQLText
+		stmtTexts = postgreSQLTexts
 	}
 
-	for id, text := range stmtText {
-		if kind == "mysql" && text != "" { // avoid loading selectModulesByIDsSQL for mysql; must be generated
+	for id, text := range stmtTexts {
+		if text != "" { // avoid loading selectModulesByIDsSQL for mysql; must be generated
 			stmt, err := db.Prepare(text)
 			if err != nil {
 				return nil, errors.Wrapf(err, "bad sql statement: %q", text)
@@ -43,7 +43,7 @@ func load(kind string, db *sql.DB) (statements, error) {
 }
 
 var (
-	mySQLText = map[int]string{
+	mySQLTexts = map[int]string{
 		insertModuleSQL:         `insert into modules(source, version) values (?, ?)`,
 		selectModuleIDSQL:       `select id from modules where source=? and version=?`,
 		selectModulesBySource:   `select id, source, version from modules where source=?`,
@@ -56,18 +56,16 @@ var (
 		selectHeartbeatsSQL:     `select hostname, port, num_modules, num_versions, unix_timestamp(ts) from proxy_heartbeats`,
 	}
 
-	postgreSQLText = map[int]string{
+	postgreSQLTexts = map[int]string{
 		insertModuleSQL:         `insert into modules (source, version) values ($1, $2)`,
 		selectModuleIDSQL:       `select id from modules where source=$1 and version=$2`,
 		selectModulesBySource:   `select id, source, version from modules where source=$1`,
 		selectModuleIDScanSQL:   `select id from modules order by id asc`,                                    // index scan module ids
 		selectModulesByIDsSQL:   `select id, source, version from modules where id=any($1) order by id asc;`, // $1 is array
 		selectSourcesScanSQL:    `select id, source, version from modules`,
-		insertHeartbeatSQL:      `insert into proxy_heartbeats (hostname, port, num_packages, num_modules) values ($1, $2, $3, $4) on conflict (hostname, port) do update set num_packages=$5, num_modules=$6, ts=current_timestamp`,
+		insertHeartbeatSQL:      `insert into proxy_heartbeats (hostname, port, num_modules, num_versions) values ($1, $2, $3, $4) on conflict (hostname, port) do update set num_modules=$5, num_versions=$6, ts=current_timestamp`,
 		insertStartupConfigSQL:  `insert into proxy_configurations (hostname, port, storage, registry, transforms) values ($1, $2, $3, $4, $5) on conflict (hostname, port) do update set storage=$6, registry=$7, transforms=$8`,
 		selectStartupConfigsSQL: `select hostname, port, storage, registry, transforms from proxy_configurations`,
-		selectHeartbeatsSQL:     `select hostname, port, num_packages, num_modules, cast(extract(epoch from ts) as integer) from proxy_heartbeats`,
+		selectHeartbeatsSQL:     `select hostname, port, num_modules, num_versions, cast(extract(epoch from ts) as integer) from proxy_heartbeats`,
 	}
 )
-
-// select cast(extract(epoch from ts) as integer) from proxy_heartbeats
